30a76dd43c5b18c6bd90a763494d6cb96a42005b
JJTacks <joel.tackey@gmail.com>
2019-02-26 22:22:07 -0500

    Updates

---
+++ precache-manifest.f9fca62635a31170c4817ec184e91ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2868d6d71bc639bb11",
+    "url": "/todo/static/css/main.951bca2e.chunk.css"
+  },
+  {
+    "revision": "2c2868d6d71bc639bb11",
+    "url": "/todo/static/js/main.2c2868d6.chunk.js"
+  },
+  {
+    "revision": "3b16784f5aabc2900b92",
+    "url": "/todo/static/js/runtime~main.3b16784f.js"
+  },
+  {
+    "revision": "8878ae2f09245461eb2f",
+    "url": "/todo/static/css/2.3b5b99ec.chunk.css"
+  },
+  {
+    "revision": "8878ae2f09245461eb2f",
+    "url": "/todo/static/js/2.8878ae2f.chunk.js"
+  },
+  {
+    "revision": "db7fbc6e7918e02e64f681f6545be954",
+    "url": "/todo/index.html"
+  }
+];
